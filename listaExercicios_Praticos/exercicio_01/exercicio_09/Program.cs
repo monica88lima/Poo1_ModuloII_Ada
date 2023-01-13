7ebb7365c63d73caa4fec0a88ecf3fd5b071d648
monica88lima <flima.monica@gmail.com>
2023-01-13 06:31:53 -0300

    exercicio de 1ao9

--- listaExercicios_Praticos/exercicio_01/exercicio_09/Program.cs
+++ listaExercicios_Praticos/exercicio_01/exercicio_09/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace exercicio_09
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace exercicio_09
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* Crie uma classe Time que tenha como propriedade o nome do país, nome do esporte e a pontuação dele 
+             em um torneio.Sua pontuação pode ser acrescida por um método pontua que recebe como parâmetro "W" para
+             vitória(+3 pontos), "L" para derrota(+0 pontos) e "D" para empate(+1 ponto).*/
+
+            Console.WriteLine("Exercicio_09\n");
+
+            Time time1 = new Time("Brasil", "São Paulo", 10);
+           
+            Console.WriteLine($"Time:{time1.NomeTime}\n Pontos Inciais:{time1.PontuacaoTime}");
+            time1.Pontuar("W");
+            time1.Pontuar("D");
+            time1.Pontuar("L");
+            time1.Pontuar("D");
+            time1.Pontuar("L");
+            time1.Pontuar("W");
+            Console.WriteLine($"Time:{time1.NomeTime}\n Pontos Final:{time1.PontuacaoTime}");
         }
     }
 }
--- listaExercicios_Praticos/exercicio_01/exercicio_09/Time.cs
+++ listaExercicios_Praticos/exercicio_01/exercicio_09/Time.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace exercicio_09
 {
-    internal class Time
+    public class Time
     {
+        public string NomePais { get; set; }
+        public string NomeTime { get; set; }
+        public int PontuacaoTime { get; set; }
+        public Time(string pais, string time, int pontos)
+        {
+            this.NomePais = pais;
+            this.NomeTime = time;
+            this.PontuacaoTime = pontos;
+        }
+
+        public void Pontuar(string status)
+        {
+            if (status.ToUpper() == "W")
+            {
+                PontuacaoTime += 3;
+            }
+            else if (status.ToUpper() == "L")
+            {
+                PontuacaoTime += 0;
+            }
+            else if (status.ToUpper() == "D")
+            {
+                PontuacaoTime += 1;
+
+            }
+            else
+            {
+                Console.WriteLine("Caracter inválido");
+            }
+        }
     }
 }
